4bd4636c09f4b09959b22eed29266692405237e6
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-05-03 06:52:48 +0300

    done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,24 @@
-export function manageFriends(state, action) {
-  // your code here
+const state = {}
+// let action = { type: "friends/add", type: "friends/remove" }
+let action = {
+  type: "friends/add",
+  payload: {
+    name: "Chrome Boi",
+    homewtown: "NYC",
+    id: 1
+  },
+  // action = {
+  type: "friends/remove",
+  payload: 1,
+  // }
+};
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "friends/add":
+      return { ...state, friends: [...state.friends, action.payload] };
+    case "friends/remove":
+      return { ...state, friends: state.friends.filter(friend => friend.id !== action.payload) };
+    default:
+      return state;
+  }
 }
